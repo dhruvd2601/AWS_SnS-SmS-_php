b81b78667eb52d365e768d194914dc7bc39bbbb2
Dhruv Modi <dhruvd2601@gmail.com>
2019-09-27 06:07:05 +0530

    add dynamic mobile numbr and message
    Link it with index.html

--- sms.php
+++ sms.php
@@ -1,5 +1,9 @@
 <?php
 require 'vendor/autoload.php';
+	$name=$_POST['Firstname'];
+	$email=$_POST['Email'];
+	$mobno=$_POST['MobileNo'];
+	$gender=$_POST['Gender'];
 	$sdk = new Aws\Sns\SnsClient([
 		'region'  => 'ap-southeast-1',
 		'version' => 'latest',
@@ -11,8 +15,8 @@ require 'vendor/autoload.php';
         ],
     ]);
 $result = $sdk->publish([
-    'Message' => 'Your Message',
-    'PhoneNumber' => 'Your Phonenumber',
+    'Message' => 'Hi '.$name.' '.$email.' '.$gender, // you can add your message in single quotes e.g. 'Hi this is my repo'.
+    'PhoneNumber' => '+91'.$mobno, // you can try default mobile number e.g. '+919876543210'
     'MessageAttributes' => ['AWS.SNS.SMS.SenderID' => [
          'DataType' => 'String',
          'StringValue' => 'WebNiraj'
